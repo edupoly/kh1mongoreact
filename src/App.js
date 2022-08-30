7273f17b49205532c73aa9e25917c728819285ca
edupoly <info.edupoly@gmail.com>
2022-08-30 11:34:47 +0530

    connection management

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react'
 function App() {
+  var [employee,setEmployee]=React.useState({
+    firstname:'',
+    lastname:'',
+    age:''
+  })
+  function addEmp(){
+    fetch("http://localhost:5500/addEmployee",{
+      method:'POST',
+      headers:{
+        'Content-type':'application/json'
+      },
+      body:JSON.stringify(employee)
+    })
+    .then((res)=>{
+      return res.json()
+    })
+    .then((data)=>{
+      console.log("data",data)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' placeholder='firstname' onKeyUp={(e)=>{setEmployee({...employee,firstname:e.target.value})}} /><br/>
+      <input type='text' placeholder='lastname'  onKeyUp={(e)=>{setEmployee({...employee,lastname:e.target.value})}}  /><br/>
+      <input type='text' placeholder='age'  onKeyUp={(e)=>{setEmployee({...employee,age:e.target.value})}} /><br/>
+      <button onClick={addEmp}>Add Employee</button>
     </div>
   );
 }
